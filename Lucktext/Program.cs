bbaa904aabba7480812b0f12203e4534bb334946
Phatchara Champathong <Phatchara.Champathong@elev.ga.ntig.se>
2023-08-30 10:42:39 +0200

    uyguykuygf

---
+++ Lucktext/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? name;
+string? food;
+string? animal;
+string? num;
+
+
+
+
+
+
+Console.WriteLine("Write name");
+name = Console.ReadLine();
+
+Console.WriteLine("Write your favorite food");
+food = Console.ReadLine();
+
+Console.WriteLine("Wrtie your favorite animal");
+animal = Console.ReadLine();
+
+Console.WriteLine("Last question, is 2 + 2 = 4?");
+num = Console.ReadLine();
+
+Console.WriteLine("In the deepest forest. You met a weird guy there.");
+Console.WriteLine("He asked your name.");
+Console.ReadKey();
+Console.WriteLine("'" + name + ", that is my name'. You answered.");
+
+
+Console.ReadKey();
