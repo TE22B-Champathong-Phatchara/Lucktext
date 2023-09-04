54a479a0f655c283e323aebbf69caec795416343
TE22B-Champathong-Phatchara <143594497+TE22B-Champathong-Phatchara@users.noreply.github.com>
2023-09-04 12:05:05 +0200

    Klar med övning

--- Lucktext/Program.cs
+++ Lucktext/Program.cs
@@ -22,10 +22,7 @@ animal = Console.ReadLine();
 Console.WriteLine("Last question, is 2 + 2 = 4?");
 num = Console.ReadLine();
 
-Console.WriteLine("In the deepest forest. You met a weird guy there.");
-Console.WriteLine("He asked your name.");
-Console.ReadKey();
-Console.WriteLine("'" + name + ", that is my name'. You answered.");
+Console.WriteLine("In the deepest forest, there is a guy named " + num + ". His favorite food is " + animal + " and he has a " + food + "as a pet. Sometime he'd like to hanging around with " + name +"!");
 
 
 Console.ReadKey();
